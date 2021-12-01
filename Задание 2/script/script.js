11b743d2f80c9e4fc353cb24c391ee28e343dd4b
GREATkuzya <greatkuzya@gmail.com>
2021-12-01 15:33:24 +0200

    add files

---
+++ Задание 2/script/script.js
@@ -0,0 +1,4 @@
+﻿let btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+  window.alert(`Ширина экрана - ${window.screen.width}, Высота экрана - ${window.screen.height}`)
+  })
